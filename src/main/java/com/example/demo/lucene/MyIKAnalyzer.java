e37034631afa2eb37ee7647407406a32c61d81b9
zzpzbl <lengpi@foxmail.com>
2021-02-09 23:55:03 +0800

    实现了搜索高亮，添加索引，以及分词

---
+++ src/main/java/com/example/demo/lucene/MyIKAnalyzer.java
@@ -0,0 +1,31 @@
+package com.example.demo.lucene;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.Tokenizer;
+
+public class MyIKAnalyzer extends Analyzer {
+
+    private boolean useSmart;
+
+    public boolean useSmart() {
+        return this.useSmart;
+    }
+
+    public void setUseSmart(boolean useSmart) {
+        this.useSmart = useSmart;
+    }
+
+    public MyIKAnalyzer() {
+        this(false);
+    }
+
+    public MyIKAnalyzer(boolean useSmart) {
+        this.useSmart = useSmart;
+    }
+
+    @Override
+    protected TokenStreamComponents createComponents(String s) {
+        Tokenizer _MyIKTokenizer = new MyIKTokenizer(this.useSmart());
+        return new TokenStreamComponents(_MyIKTokenizer);
+    }
+}
